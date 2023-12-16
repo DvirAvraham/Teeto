35327264225d458925189c43d4d4e8a1bd231a91
DvirAvraham <dvir6g6@gmail.com>
2023-12-16 19:14:25 +0200

    empty params fix

--- endpoint_finder.js
+++ endpoint_finder.js
@@ -72,7 +72,7 @@ function getFormsParams(){
     for (let j = 0; j < formElements.length; j++) {
       const element = formElements[j];
       
-      if (element.type !== 'submit') {
+      if (element.type !== 'submit' && !!element.name) {
         let paramName = element.name
         !paramNameSet.some(param=>param === paramName) && paramNameSet.push(paramName); 
       }
@@ -94,7 +94,7 @@ function getUrlParams(url) {
 
     while ((match = paramsRegex.exec(url)) !== null) {
       const paramName = decodeURIComponent(match[1]);
-      !paramNameSet.some(param=>param === paramName) && paramNameSet.push(paramName); 
+      if(!!paramName && !paramNameSet.some(param=>param === paramName)) paramNameSet.push(paramName); 
     }
 }
 
